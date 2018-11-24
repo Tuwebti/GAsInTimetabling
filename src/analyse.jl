d510012bf86418d1c6974e7e1f022a94e48f6bb4
Malik Tuwebti <tuwebti.malik@gmail.com>
2018-11-24 11:44:50 +0000

    changed backend for plotting

--- src/analyse.jl
+++ src/analyse.jl
@@ -5,7 +5,7 @@ abstract type BeginhookAlg <: Algorithm end
 abstract type EndhookAlg <: Algorithm end
 abstract type IteratehookAlg <: Algorithm end
 struct CollectMeanScore <: IteratehookAlg end
-
+plotlyjs()
 
 abstract type AnalyseState end #a variable of type AnalyseState collects information during the execution in order to perform analysis at the end.
 #-----------------
@@ -25,15 +25,6 @@ function _displayresults(::CollectMeanScore)
     plot(meanScores, xlabel="iterations" , ylabel="score" , label="mean score")
 end
 
-# The following shows a heatmap of the constraints between events
-function constraintHook(constraints::EdgeConstraints)
-    events=["MTH1001","MTH1002","MTH1003","MTH1004","MTH2001","MTH2002","MTH2003","MTH2004"] #this is a temporary hack, need to make events into vector
-    for i in 1:length(events)
-        for j in 1:length(events)
-            clashes[i,j]=constraints.edges[(events[i],events[j])]
-        end
-    end
-end
 
 
 
