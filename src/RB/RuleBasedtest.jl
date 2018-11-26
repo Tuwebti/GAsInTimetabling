09cd206959e9af0fd5db7b9f3f58110a9d4d5aa5
Ethan Jones <ethan_dedi@outlook.com>
2018-11-26 17:31:30 +0000

    Hard Constraint as a function

--- src/RB/RuleBasedtest.jl
+++ src/RB/RuleBasedtest.jl
@@ -1,23 +1,24 @@
 #TODO create an array for bipartite connections, make plots
 #TODO implement rooms size constraint
-#TODO count the number of students & modules assigned to not assigned
+#TODO implement a clique/degree strategy? Then retimetable those modules
 #TODO GRAPHS:
 #(1) number of slots against no. of students assigned
 #(2) bipartite connections
 
-function DeterminsticMain()
-    noSlots = 55
+function DeterminsticMain(noSlots)
     slotsFree = zeros(noSlots)
     timeSlots = zeros(noSlots)
     studentsByModule = timetablingProblem.studentsByModule
     studentsByTimeslot = Dict{String,Set{Student}}()
     modulesByTimeslot = Dict{String,Set{Event}}()
     timeSlotAvailableTEMP = Dict{String,Set{Event}}()
+    roomAvailability = Dict{String,Set{Event}}()
     #Initialise timeslots
     for i in 1:noSlots
         timeslot = string(i)
         studentsByTimeslot[timeslot] = Set()
         modulesByTimeslot[timeslot] = Set()
+        #roomAvailability[timeslot] = =Set()
     end
 
     for event in keys(studentsByModule)
@@ -33,13 +34,22 @@ function DeterminsticMain()
             if  ! isempty(intersect(studentsByModule[event], studentsByTimeslot[timeslot]))
                 #we have a clash
             else
+                #roomChecker = Array{Int64}
+                #for roomSize in get(roomAvailability,timeslot,0)
+                    #check if room size is available for module
+                    #if false
+                        #here we assign the room as possible
+                    #    append(roomChecker, roomSize)
+                    #end
+                #end
+                #at end of loop we assign module to smallest room
+
                 #add event to available timeslot
-                #calculate soft constraint score here?
                 union!(timeSlotAvailableTEMP[timeslot],[event])
+                #SOFT CONSTRAINT HERE
             end
         end
-        #Soft Constraints, searches through available timeslots and scores them
-        #then chooses most preferrable timeSlot
+        #Soft Constraints FIX
         for availableSlot in sort(collect(keys(timeSlotAvailableTEMP)))
             if ! isempty(get(timeSlotAvailableTEMP,availableSlot,0))
                 union!(modulesByTimeslot[availableSlot],[event])
@@ -48,8 +58,7 @@ function DeterminsticMain()
             end
         end
     end
-    #print modulesByTimeslot in order, and counts modules and students
-    noStudentsAssigned = 0
+    #print modulesByTimeslot in order, and counts modules
     noModulesAssigned = 0
     for key in map(x -> string(x),sort(map(x -> parse(Int, x), collect(keys(modulesByTimeslot)))))
         print("timeslot " * key * " : ")
@@ -57,8 +66,11 @@ function DeterminsticMain()
         #count the amount of students
         noModulesAssigned += length(get(modulesByTimeslot,key,0))
         #count the amount of modules
-        noStudentsAssigned += length(get(studentsByTimeslot,key,0))
         println()
     end
-    #count the amount of students & modules
+    #print the amount of modules unassigned for dataset 1
+    noModulesUnassigned = 400-noModulesAssigned
+    println("Number of Modules Unassigned: ",noModulesUnassigned)
+    return modulesByTimeslot
 end
+modulesByTimeslot = DeterminsticMain(55)
