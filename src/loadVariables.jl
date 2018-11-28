7848d96fcdb3e985c0a5f8da03f59ef43770f257
Malik Tuwebti <tuwebti.malik@gmail.com>
2018-11-28 17:22:30 +0000

    added different plots

--- src/loadVariables.jl
+++ src/loadVariables.jl
@@ -1,2 +1,9 @@
-chromosomes = load("saved-variables/test.jld","chromosomes")
-meanScores = load("saved-variables/test.jld","meanScores")
+println("available files are :")
+filenames=readdir("saved-variables")
+for (i,filename) in enumerate(filenames)
+    println(string(i) *" : "* filename)
+end
+println("specify file to load from :")
+saveFile = filenames[parse(Int,readline(stdin))]
+chromosomes = load(joinpath("saved-variables",saveFile),"chromosomes")
+meanScores = load(joinpath("saved-variables",saveFile),"meanScores")
