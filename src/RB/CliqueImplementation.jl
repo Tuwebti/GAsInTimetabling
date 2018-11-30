49161b529a56dcae06279182f0763d19b8da04c7
Will <willashton51@gmail.com>
2018-11-30 18:03:26 +0000

    Uploaded Clique Algorithm

--- src/RB/CliqueImplementation.jl
+++ src/RB/CliqueImplementation.jl
@@ -1,3 +1,25 @@
 #TODO implement a clique/degree strategy Then retimetable those modules
 function cliqueStrategy()
+
+    dataset = copy(timetablingProblem.studentsByModule)
+    N = 10
+
+    lowestscoretimetable = dataset
+    comparetimetable = copy(timetablingProblem.studentsByModule)
+    #comparetimetable = deleteat!(lowestscoretimetable,findin(lowestscoretimetable,mostconstrainedModule))
+    DeterministicMain(lowestscoretimetable)
+    lowestscore = 100
+
+    for i from 1:N
+        modremove = mostconstrainedModule(comparetimetable)
+        comparetimetable = deleteat!(comparetimetable,findin(comparetimetable,modremove))
+        DeterministicMain(comparetimetable)
+        comparescore[i] = length(unnasignedmodules)
+        if comparescore[i] < lowestscore
+            lowestscoretimetable = comparetimetable
+            lowestscore = comparescore[i]
+            lowestscoreUModules = unassignedModules
+        end
+    end
+    return(lowestscoretimetable,unassignedModules)
 end
